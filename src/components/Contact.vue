48bec2ce9c82ba50fe73dd256c245f0e877711a2
Can Yapan <canyapan@gmail.com>
2025-03-21 04:33:51 -0400

    Init (#1)
    * initial commit with basic functionality
    * modified .gitignore to ignore all under .vscode folder

---
+++ src/components/Contact.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="p-6">
+      <h2 class="text-2xl font-semibold mb-4">Contact</h2>
+      <p>LinkedIn: <a href="https://linkedin.com/in/canyapan" class="text-blue-500">linkedin.com/in/canyapan</a></p>
+    </div>
+  </template>
---
+++ src/components/Skills.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="p-6">
+      <h2 class="text-2xl font-semibold mb-4">Skills</h2>
+      <ul class="list-disc pl-6">
+        <li>Java, Spring Boot</li>
+        <li>MQ, DB, AWS, K8s, and more...</li>
+      </ul>
+    </div>
+  </template>
