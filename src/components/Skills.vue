8294f080a9384029aff41b787680ae619c5cea95
Can Yapan <canyapan@gmail.com>
2025-06-21 19:37:09 -0400

    fixed merge with conflicts (#10)
    Co-authored-by: John Yapan <johnyapan@gmail.com>

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,11 +3,7 @@
     <h2>Skills</h2>
     <ul>
       <li>Java, Spring Boot</li>
-<<<<<<< HEAD
       <li>MQ, DBs, AWS, K8s, and more...</li>
-=======
-      <li>MQ, DB, AWS, K8s, and more...</li>
->>>>>>> origin/main
     </ul>
   </div>
 </template>
